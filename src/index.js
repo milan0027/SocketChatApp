5791645ccb7ac88c93ff1526b4dcf94fe67a5b47
Milan Mundhra <78230444+milan0027@users.noreply.github.com>
2021-09-16 02:21:49 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app  =  express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const port = process.env.port  || 3000
+const port = process.env.PORT  || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
